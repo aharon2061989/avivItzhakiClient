35733f07750e4aba7b5c0c08adefd6a3f6d66ff4
aharon ben shimol <ahron2061989@gmail.com>
2024-07-28 01:56:20 +0300

    hamburger menu added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,41 @@
 import React from "react";
 import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 
 function Footer() {
-    return(
-<footer>
-    <div className="footer-container">
-        <div className="footerSection">
-            <p><b style={{color:"white"}}>© 2024 כל הזכויות שמורות לאביב יצחקי</b></p>
-        </div>
-        <div className="footerSection">
-            <h4>צור קשר</h4>
-            <p>טלפון: <a href="tel:+97212345678">050-406-6900</a></p>
-            <p style={{direction:"rtl"}}><b style={{color:"white"}}>דוא"ל: </b><a href="mailto:avivtaxes@gmail.com">avivtaxes@gmail.com</a></p>
-        </div>
-        <div className="footerSection">
-            <h4>קישורים חשובים</h4>
-            <ul>
-                <li><Link to="/InformationGeneral">מידע כללי</Link></li>
-                <li><Link to="/ContactPage">צור קשר</Link></li>
-                <li><Link to="/PrivacyPolicy">מדיניות פרטיות ותנאי שימוש</Link></li>
-            </ul>
-        </div>
-        <div className="footerSection">
-            <h4>עקבו אחרי</h4>
-            <br/>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2xl" style={{color: "#1877F7",}} /></a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} size="2xl" style={{color: "#f773c7",}} /></a>
-        </div>
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 
-    </div>
-</footer>
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className="footerSection">
+                    <p><b style={{color:"white"}}>© 2024 כל הזכויות שמורות לאביב יצחקי</b></p>
+                </div>
+                <div className="footerSection">
+                    <h4>צור קשר</h4>
+                    <p>טלפון: <a href="tel:+97212345678">050-406-6900</a></p>
+                    <p style={{direction:"rtl"}}><b style={{color:"white"}}>דוא"ל: </b><a href="mailto:avivtaxes@gmail.com">avivtaxes@gmail.com</a></p>
+                </div>
+                <div className="footerSection">
+                    <h4>קישורים חשובים</h4>
+                    <ul>
+                        <li><Link to="/InformationGeneral#top" onClick={scrollToTop}>מידע כללי</Link></li>
+                        <li><Link to="/ContactPage" onClick={scrollToTop}>צור קשר</Link></li>
+                        <li><Link to="/PrivacyPolicy" onClick={scrollToTop}>מדיניות פרטיות ותנאי שימוש</Link></li>
+                    </ul>
+                </div>
+                <div className="footerSection">
+                    <h4>עקבו אחרי</h4>
+                    <br />
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2xl" style={{color: "#1877F7"}} /></a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} size="2xl" style={{color: "#f773c7"}} /></a>
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './NavbarComponent.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faInstagram, faSquareWhatsapp} from '@fortawesome/free-brands-svg-icons'; 
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faSquareWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { Link } from "react-router-dom";
 
 function NavbarComponent() {
+    const [burgerClass, setBurgerClass] = useState("burgerBar unclicked");
+    const [menuClass, setMenuClass] = useState("menu hidden");
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+    const updateMenu = () => {
+        if(!isMenuClicked) {
+            setBurgerClass("burgerBar clicked");
+            setMenuClass("menu visible");
+            document.body.style.overflow = 'hidden';
+        }
+        else {
+            setBurgerClass("burgerBar unclicked");
+            setMenuClass("menu hidden");
+            document.body.style.overflow = 'auto';
+        }
+        setIsMenuClicked(!isMenuClicked);
+    }
+
     return(
         <div>
             <div className="navbar">
@@ -15,32 +33,42 @@ function NavbarComponent() {
                     <a href="https://wa.me/972504066900" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSquareWhatsapp} size="2xl" style={{color: "#028860",}}/></a>
                     <div className="phonemailIcons">
                         <a className="phone" href="tel:+972504066900"><h4>050-406-6900</h4></a>
-                        <FontAwesomeIcon className="icon" icon={faPhone} flip="horizontal"
-                        size="xl" style={{"--fa-secondary-color": "#ffffff",}}/>
-                        
+                        <FontAwesomeIcon className="icon" icon={faPhone} flip="horizontal" size="xl" style={{"--fa-secondary-color": "#ffffff",}}/>
                         <a className="email" href="mailto:avivtaxes@gmail.com"><h4>avivtaxes@gmail.com</h4></a>
                         <FontAwesomeIcon className="icon" icon={faEnvelope} size="xl"/>
                     </div>
-
                 </div>
                 <div className="rightIcons">
                     <div className="links">
                         <div className="dropdown">
                             <Link className="secondNav" id="information"><h3>מידע</h3></Link>
-                        <div className="dropdown-content">
-                            <Link to="/SeverancePayInfo">החזר עבור משיכת פיצויים</Link>
-                            <Link to="/EducationFundInfo">החזר עבור משיכת קרן השתלמות</Link>
-                            <Link to="/PensionInfo">החזר עבור משיכת פנסיה</Link>
-                            <Link to="/InformationGeneral">החזרי מס כללי</Link>
+                            <div className="dropdown-content">
+                                <Link to="/SeverancePayInfo">החזר עבור משיכת פיצויים</Link>
+                                <Link to="/EducationFundInfo">החזר עבור משיכת קרן השתלמות</Link>
+                                <Link to="/PensionInfo">החזר עבור משיכת פנסיה</Link>
+                                <Link to="/InformationGeneral">החזרי מס כללי</Link>
+                            </div>
                         </div>
-                    </div>
-                        <Link to='/Forms' className="secondNav"><h3>טפסים להורדה</h3></Link>
                         <Link to='/ContactPage' className="secondNav"><h3>צור קשר</h3></Link>
                         <Link to='/' className="secondNav" id=""><h3>ראשי</h3></Link>
                     </div>
-
-
                 </div>
+                <div className="burgerMenu" onClick={updateMenu}>
+                    <div className={burgerClass}></div>
+                    <div className={burgerClass}></div>
+                    <div className={burgerClass}></div>
+                </div>
+            </div>
+            <div className={menuClass} id="menu">
+                <Link to='/' onClick={updateMenu}>ראשי</Link>
+                <Link to='/ContactPage' onClick={updateMenu}>צור קשר</Link>
+                <Link>מידע</Link> 
+
+
+                 <Link to="/SeverancePayInfo" onClick={updateMenu}>החזר על משיכת פיצויים</Link>
+                <Link to="/EducationFundInfo" onClick={updateMenu}>החזר על קרן השתלמות</Link>
+                <Link to="/PensionInfo" onClick={updateMenu}>החזר על משיכת פנסיה</Link>
+                <Link to="/InformationGeneral" onClick={updateMenu}>החזרי מס כללי</Link>
             </div>
         </div>
     );
